007ba914e0f3f31e5726e2a9437b1921d10d1684
APKakuja <adrianpetrisorkakuja@gmail.com>
2025-12-18 19:55:25 +0100

    Commit

--- 01-Fils/Fil.java
+++ 01-Fils/Fil.java
@@ -3,17 +3,14 @@
 public class Fil extends Thread {
     private String nom;
     private int iteracions;
-    private int prioritat;
-    private int delayMs;
     private int bucleSimulacio;
-   
-    public Fil(String nom, int iteracions, int prioritat, int delayMs, int bucleSimulacio) {
+    private int delayMs;
+    
+    public Fil(String nom, int iteracions, int prioritat, int bucleSimulacio, int delayMs) {
         this.nom = nom;
         this.iteracions = iteracions;
-        this.prioritat = prioritat;
-        this.delayMs = delayMs;
         this.bucleSimulacio = bucleSimulacio;
-        
+        this.delayMs = delayMs;
         this.setPriority(prioritat);
     }
     
@@ -21,22 +18,22 @@ public class Fil extends Thread {
     public void run() {
         try {
             for (int i = 1; i <= iteracions; i++) {
-                
+                // Simular feina amb el bucle
                 for (int j = 0; j < bucleSimulacio; j++) {
-                    Math.sqrt(j); 
+                    Math.sqrt(j * j + j);
                 }
                 
                 System.out.println(nom + " " + i);
                 
+                // Delay si cal
                 if (delayMs > 0) {
                     Thread.sleep(delayMs);
+                    Thread.yield(); // Cedir explícitament el torn
                 }
             }
-            
             System.out.println("Acaba el fil " + nom);
-            
         } catch (InterruptedException e) {
-            System.err.println("El fil " + nom + " ha estat interromput");
+            e.printStackTrace();
         }
     }
 }
--- 01-Fils/PrincipalEstricte.java
+++ 01-Fils/PrincipalEstricte.java
@@ -2,13 +2,14 @@
 
 public class PrincipalEstricte {
     public static void main(String[] args) {
+        Fil juan = new Fil("Juan", 9, Thread.NORM_PRIORITY, 1, 10);
+        Fil pepe = new Fil("Pepe", 9, Thread.NORM_PRIORITY, 1, 10);
         
-        Fil juan = new Fil("Juan", 9, Thread.NORM_PRIORITY, 3, 0);
-        Fil pepe = new Fil("Pepe", 9, Thread.NORM_PRIORITY, 3, 0);
-        
+
         juan.start();
         pepe.start();
         
-        System.out.println("Termina thread main");
+        
+        System.out.println("Acaba thread main");
     }
 }
