a166afd48d08a4dd9b51d452b0ff29edd49836d2
APKakuja <adrianpetrisorkakuja@gmail.com>
2026-01-29 12:10:33 +0100

    Commit final amb totas las implementacións necesaries

--- 05-Synchronized/src/Associacio.java
+++ 05-Synchronized/src/Associacio.java
@@ -5,6 +5,37 @@ public class Associacio {
 
     public Associacio() {
         socis = new Soci[numSocis];
+        for (int i = 0; i < numSocis; i++) {
+            socis[i] = new Soci();
+        }
+    }
+
+    public void iniciaCompteTempsSocis() {
+        for (Soci s : socis) {
+            s.start();
+        }
+    }
+
+    public void esperaPeriodeSocis() {
+        for (Soci s : socis) {
+            try {
+                s.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void mostraBalanceComptes() {
+        float saldoFinal = Compte.getInstance().getSaldo();
+        System.out.printf("Saldo final del compte: %.2f Euros\n", saldoFinal);
+    }
+
+    public static void main(String[] args) {
+        Associacio a = new Associacio();
+        a.iniciaCompteTempsSocis();
+        a.esperaPeriodeSocis();
+        a.mostraBalanceComptes();
     }
 
 }
